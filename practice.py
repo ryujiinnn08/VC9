2755ba7be0bc34ac0cc4f448e1028966a85501cc
aliya-prtty <liyancastaneda@gmail.com>
2025-05-31 20:29:44 +0800

    Add practice.py after cloning

---
+++ practice.py
@@ -0,0 +1,8 @@
+# Practice file created after cloning
+def main():
+    """Main function for practice"""
+    print("This file was created after cloning the repository!")
+    print("Demonstrating local changes and push to remote.")
+
+if __name__ == "__main__":
+    main()
